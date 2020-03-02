61ee69f2dcca9287310b56435b128cd1ceeca432
Juferoga <juancholoco2000.61@gmail.com>
2020-03-01 19:46:27 -0500

    Upload

--- src/Componentes/Ventana_Dialogo.java
+++ src/Componentes/Ventana_Dialogo.java
@@ -17,7 +17,7 @@ public class Ventana_Dialogo {
      * @param parentComponent : Componente Padre
      * @param mess : Mensaje a mostar
      */    
-    public static void message(Component parentComponent, String mess) {
+    public static void message(final Component parentComponent, final String mess) {
         JOptionPane.showMessageDialog(parentComponent, mess, "Mensaje", JOptionPane.PLAIN_MESSAGE);
     }
 
@@ -25,9 +25,9 @@ public class Ventana_Dialogo {
      * Mensaje de error
      *
      * @param parentComponent : Componente Padre
-     * @param mess : Mensaje a mostar
+     * @param mess            : Mensaje a mostar
      */
-    public static void erreur(Component parentComponent, String mess) {
+    public static void erreur(final Component parentComponent, final String mess) {
         JOptionPane.showMessageDialog(parentComponent, mess, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
@@ -35,9 +35,9 @@ public class Ventana_Dialogo {
      * Mensaje de peligro
      *
      * @param parentComponent : Componente Padre
-     * @param mess : Mensaje a mostar
+     * @param mess            : Mensaje a mostar
      */
-    public static void danger(Component parentComponent, String mess) {
+    public static void danger(final Component parentComponent, final String mess) {
         JOptionPane.showMessageDialog(parentComponent, mess, "Peligro", JOptionPane.WARNING_MESSAGE);
     }
 
@@ -45,9 +45,9 @@ public class Ventana_Dialogo {
      * Mensaje de Alerta
      *
      * @param parentComponent : Componente Padre
-     * @param mess : Mensaje a mostar
+     * @param mess            : Mensaje a mostar
      */
-    public static void attention(Component parentComponent, String mess) {
+    public static void attention(final Component parentComponent, final String mess) {
         JOptionPane.showMessageDialog(parentComponent, mess, "Antencion!!", JOptionPane.WARNING_MESSAGE);
     }
 
@@ -55,25 +55,24 @@ public class Ventana_Dialogo {
      * Mensaje de Informacion
      *
      * @param parentComponent : Componente Padre
-     * @param mess : Mensaje a mostar
+     * @param mess            : Mensaje a mostar
      */
-    public static void info(Component parentComponent, String mess) {
-        JOptionPane.showMessageDialog(parentComponent, mess, "Información", JOptionPane.INFORMATION_MESSAGE);
+    public static void info(final Component parentComponent, final String mess) {
+        JOptionPane.showMessageDialog(parentComponent, mess, "Información",
+         JOptionPane.INFORMATION_MESSAGE);
     }
 
     /**
      * Caja de dialogo que Pide ingresar un texto
      *
      * @param parentComponent : Componente Padre
-     * @param mess : Mensaje a mostar
-     * @param title : El titulo del Mensaje
+     * @param mess            : Mensaje a mostar
+     * @param title           : El titulo del Mensaje
      * @return Retorna el texto ingresado por el usuario
      */
-    public static String prompt(Component parentComponent, String mess, String title) {
-        String rep = JOptionPane.showInputDialog(parentComponent,
-                mess,
-                title,
-                JOptionPane.QUESTION_MESSAGE);
+    public static String prompt(final Component parentComponent, final String mess, final String title) {
+        final String rep = JOptionPane.showInputDialog(parentComponent, mess, title,
+         JOptionPane.QUESTION_MESSAGE);
 
         return rep;
     }
@@ -82,13 +81,16 @@ public class Ventana_Dialogo {
      * Caja de dialogo con un boton de OK y un boton CANCEL
      *
      * @param parentComponent : Componente Padre
-     * @param mess : Mensaje a mostar
+     * @param mess            : Mensaje a mostar
      * @return retorna true si se clikea sobre OK y false en el caso opuesto
      */
-    public static boolean confirm(Component parentComponent, String mess) {
-        int rep = JOptionPane.showConfirmDialog(parentComponent, mess, "Confirmar", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+    public static boolean confirm(final Component parentComponent, final String mess) {
+        final int rep = JOptionPane.showConfirmDialog(parentComponent,
+         mess, "Confirmar", JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
 
-        if (rep == JOptionPane.CANCEL_OPTION || rep == JOptionPane.CLOSED_OPTION || rep == JOptionPane.NO_OPTION) {
+        if (rep == JOptionPane.CANCEL_OPTION || rep == JOptionPane.CLOSED_OPTION || 
+        rep == JOptionPane.NO_OPTION) {
             return false;
         }
         return true;
@@ -98,27 +100,30 @@ public class Ventana_Dialogo {
      * Caja de dialogo con un boton de YES y un boton NO
      *
      * @param parentComponent : Componente Padre
-     * @param mess : Mensaje a mostar
+     * @param mess            : Mensaje a mostar
      * @return retorna true si se clikea sobre YES y false en el caso opuesto
      */
-    public static boolean yesNo(Component parentComponent, String mess) {
-        int rep = JOptionPane.showConfirmDialog(parentComponent, mess, "Confirmar", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+    public static boolean yesNo(final Component parentComponent, final String mess) {
+        final int rep = JOptionPane.showConfirmDialog(parentComponent, mess, "Confirmar",
+         JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
 
-        if (rep == JOptionPane.NO_OPTION || rep == JOptionPane.CLOSED_OPTION || rep == JOptionPane.NO_OPTION) {
+        if (rep == JOptionPane.NO_OPTION || rep == JOptionPane.CLOSED_OPTION || 
+        rep == JOptionPane.NO_OPTION) {
             return false;
         }
         return true;
     }
 
     /**
-     * Caja de dialogo con un boton de YES, un boton NO y un boton
-     * CANCEL
+     * Caja de dialogo con un boton de YES, un boton NO y un boton CANCEL
      *
      * @param parentComponent : Componente Padre
      * @return Devuelve en el caso opuesto, 0 para CANCEL y 1 para YES
      */
-    public static int yesNoCancel(Component parentComponent, String mess) {
-        int rep = JOptionPane.showConfirmDialog(parentComponent, mess, "Confirmar", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+    public static int yesNoCancel(final Component parentComponent, final String mess) {
+        final int rep = JOptionPane.showConfirmDialog(parentComponent, mess, "Confirmar",
+                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
 
         if (rep == JOptionPane.CLOSED_OPTION || rep == JOptionPane.CANCEL_OPTION) {
             return 0;
@@ -137,8 +142,11 @@ public class Ventana_Dialogo {
      * @deprecated
      * @see #listDialog(Component, String, String, String[])
      */
-    public static int multiChoix(Component parentComponent, String mess, String titre, String[] choix) {
-        int n = JOptionPane.showOptionDialog(parentComponent, mess, titre, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, choix, "Cancelar");
+    public static int multiChoix(final Component parentComponent, final String mess, final String titre,
+            final String[] choix) {
+        final int n = JOptionPane.showOptionDialog(parentComponent, mess, titre,
+         JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, choix, "Cancelar");
 
         return n;
     }
@@ -147,13 +155,15 @@ public class Ventana_Dialogo {
      * Caja de dialogo con caja de eleccion (ListBox)
      *
      * @param parentComponent : Componente Padre
-     * @param mess : Mensaje a mostar
-     * @param title : Titulo de la caja de dialogo
-     * @param list : Una lista forma de String[]
+     * @param mess            : Mensaje a mostar
+     * @param title           : Titulo de la caja de dialogo
+     * @param list            : Una lista forma de String[]
      * @return Retorna la eleccion del usuario en forma de String
      */
-    public static String listDialog(Component parentComponent, String mess, String title, String[] list) {
-        String str = (String) JOptionPane.showInputDialog(parentComponent, mess, title, JOptionPane.QUESTION_MESSAGE, null, list, "null");
+    public static String listDialog(final Component parentComponent, final String mess,
+     final String title, final String[] list) {
+        final String str = (String) JOptionPane.showInputDialog(parentComponent, mess, title,
+                JOptionPane.QUESTION_MESSAGE, null, list, "null");
         return str;
     }
 }
